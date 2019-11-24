b6c3bbf43b24bc403c9fc403378d3e4952e96aeb
zsg-github <463798845@qq.com>
2019-11-24 14:53:55 +0800

    student222

---
+++ src/main/java/com/ssm/dao/TeacherMapper.java
@@ -0,0 +1,9 @@
+package com.ssm.dao;
+
+import com.ssm.entity.Student;
+
+public interface TeacherMapper {
+
+    void addTeacherById(Student student222);
+    void addTeacherByName(Student student222);
+}
